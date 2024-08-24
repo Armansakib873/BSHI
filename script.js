b682946abb3ffb103da2e8fbc894bf65ba9d0d28
Md. Arman Hossen <armanhossen873@gmail.com>
2024-08-25 01:21:42 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
