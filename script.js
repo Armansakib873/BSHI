43b02e5a769c81e016a665386bd09db2d51140ca
Md. Arman Hossen <armanhossen873@gmail.com>
2024-08-26 18:35:12 +0600

    ready

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.table-seba tbody tr').forEach(function(row) {
-    row.addEventListener('mouseenter', function() {
-        const hoverSound = document.getElementById('hoverSound');
-        hoverSound.currentTime = 0; // Reset the audio to the beginning
-        hoverSound.play();
-    });
-});
